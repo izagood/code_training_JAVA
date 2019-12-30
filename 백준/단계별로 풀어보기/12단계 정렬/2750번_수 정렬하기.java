29248615fcfef0a022629986c420ba2c05bd3819
Zagood <34632249+izagood@users.noreply.github.com>
2019-12-30 17:37:30 +0900

    Create 2750번_수 정렬하기.java

---
+++ 백준/단계별로 풀어보기/12단계 정렬/2750번_수 정렬하기.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int input = Integer.parseInt(br.readLine());
+        int[] arr = new int[input];
+
+        // 배열값 할당
+        for(int i=0; i<input; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        //버블정렬
+        int temp;
+        for(int i = 0; i < input-1; i++){
+            for(int j = input - 1; j > i; j--){
+                if(arr[j - 1]>arr[j]){
+                    temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        // 정렬된 배열 출력
+        for(int i=0; i<input; i++){
+            System.out.println(arr[i]);
+        }
+
+
+    }
+}
