5bfcf31f38b87e1c4cc2b7781c8fea0e775add65
Zagood <34632249+izagood@users.noreply.github.com>
2020-01-09 14:57:46 +0900

    Create ScoreManagement.java

---
+++ 알고리즘 수업/2주차/3일/03_hashmap_성적표만들기연습/ScoreManagement.java
@@ -0,0 +1,19 @@
+package week02.d3.ch5;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ScoreManagement { //해시 맵을 이용해 성적표 만들기
+
+    Map<String, Student> data = new HashMap<>();
+
+    void genStudent(){
+        
+    }
+
+    public static void main(String[] args) {
+        ScoreManagement app = new ScoreManagement();
+        app.genStudent();
+
+    }
+}
