b8413fe0479e2630493f50985cbdf04e706a22c7
Zagood <34632249+izagood@users.noreply.github.com>
2020-02-13 15:08:58 +0900

    Add files via upload

---
+++ 비트교육센터-JAVA-programming/day9/BIT_app.day8/src/review/classExLang/WordCount1.java
@@ -0,0 +1,20 @@
+package review.classExLang;
+import java.util.*;
+
+public class WordCount1 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		while(true) {
+			System.out.print(">>");
+			String s = scanner.nextLine();
+			if(s.equals("그만")) {
+				System.out.println("종료합니다...");	
+				break;
+			}
+			String [] words = s.split(" ");	
+			System.out.println("어절 개수는 " + words.length);
+		}
+		
+		scanner.close();
+	}
+}
