a885aad1a0aedb45649430faec499983e77f1aa8
zagood <ljbfif50@gmail.com>
2021-03-25 17:58:54 +0900

    테스트 백업

--- project/baekjoon_proj/src/baekjoon_proj/problems/stepbystep/no2/TwoNumer_1330.java
+++ project/baekjoon_proj/src/baekjoon_proj/problems/stepbystep/no2/TwoNumer_1330.java
@@ -1,5 +1,21 @@
 package baekjoon_proj.problems.stepbystep.no2;
 
-public class TwoNumer_1330 {
+import java.io.IOException;
+import java.util.Scanner;
 
+public class TwoNumer_1330 {
+	public void twoNumber() throws IOException {
+		Scanner reader = new Scanner(System.in);
+		
+		int num1 = reader.nextInt();
+		int num2 = reader.nextInt();
+		
+		if(num1 < num2) {
+			System.out.println("<");
+		}else if(num1 > num2) {
+			System.out.println(">");
+		}else {
+			System.out.println("==");
+		}
+	}
 }
--- project/baekjoon_proj/src/test/problems/Tests.java
+++ project/baekjoon_proj/src/test/problems/Tests.java
@@ -1,23 +1,41 @@
 package test.problems;
 
+import java.io.IOException;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import baekjoon_proj.problems.stepbystep.no1.HelloWorld_2557;
 import baekjoon_proj.problems.stepbystep.no1.WeLoveKriii_10718;
+import baekjoon_proj.problems.stepbystep.no2.Grade_9498;
+import baekjoon_proj.problems.stepbystep.no2.TwoNumer_1330;
 
 public class Tests {
 	@Test
 	@DisplayName("기본 출력")
-	void helloWorldTest() {
+	void helloWorld_2557() {
 		HelloWorld_2557 helloWorld = new HelloWorld_2557();
 		helloWorld.helloWorld();
 	}
 	
 	@Test
 	@DisplayName("기본 출력")
-	void WeLoveKriiiTest() {
+	void weLoveKriii_10718() {
 		WeLoveKriii_10718 weLoveKriii = new WeLoveKriii_10718();
 		weLoveKriii.weLoveKriii();
+	}
+	
+	@Test
+	@DisplayName("기본 출력")
+	void twoNumer_1330() throws IOException {
+		TwoNumer_1330 twoNumer_1330 = new TwoNumer_1330();
+		twoNumer_1330.twoNumber();
+	}
+	
+	@Test
+	@DisplayName("성적 출력")
+	void grade_9498() throws IOException {
+		Grade_9498 grade_9498 = new Grade_9498();
+		grade_9498.grade();
 	}
 }
