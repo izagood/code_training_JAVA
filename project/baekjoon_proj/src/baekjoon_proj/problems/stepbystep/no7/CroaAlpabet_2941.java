e75604a7e4fd73fe9ce5cd5ffd24f5ce289e4f25
zagood <ljbfif50@gmail.com>
2021-04-05 18:00:27 +0900

    백업

--- project/baekjoon_proj/src/baekjoon_proj/problems/stepbystep/no7/CroaAlpabet_2941.java
+++ project/baekjoon_proj/src/baekjoon_proj/problems/stepbystep/no7/CroaAlpabet_2941.java
@@ -1,8 +1,18 @@
 package baekjoon_proj.problems.stepbystep.no7;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class CroaAlpabet_2941 {
 
 	public void cora() {
+		String pattern = ".*c=.*";
 		
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.next();
+		scanner.close();
+		
+		boolean matches = Pattern.matches(pattern, input);
+		System.out.println(matches);
 	}
 }
