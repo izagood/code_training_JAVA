9e8d18c704f0627f92de9330cc55512bf6d8324c
zagood <ljbfif50@gmail.com>
2021-06-04 08:57:53 +0900

    백업

--- project/baekjoon_proj/src/baekjoon_proj/problems/stepbystep/no7/GroupWordCheck_1316.java
+++ project/baekjoon_proj/src/baekjoon_proj/problems/stepbystep/no7/GroupWordCheck_1316.java
@@ -1,9 +1,21 @@
 package baekjoon_proj.problems.stepbystep.no7;
 
-public class GroupWordCheck_1316 {
+import java.util.Scanner;
 
+public class GroupWordCheck_1316 {
+	
+	// 알파벳이 연속되거나 한번만 나와야 함
 	public void check() {
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
 		
+		
+		
+		for(int i = 0; i < num; i++) {
+			String word = scanner.next();
+			
+		}
+		scanner.close();
 	}
 
 }
